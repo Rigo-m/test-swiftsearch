caa2899f2968ac3c5221d83e1fc298409e409f66
Matteo Rigoni <matteo.rigoni@atoms.studio>
2024-05-03 11:43:58 +0200

    page redirection

---
+++ middleware/00.page.global.ts
@@ -0,0 +1,46 @@
+import qs from "qs";
+
+const removePageFromUrl = (url: string) => {
+  // Use a regular expression to remove any '/page/:id' part from the URL
+  return url.replace(/\/page\/\d+/, "");
+};
+
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  if (import.meta.client) return;
+
+  const isPaginationPage =
+    to.name === "withPage-page-page" || to.name === "withPage";
+
+  if (!isPaginationPage) return;
+
+  const { query, path, params, name } = to;
+  const pageValue = query?.page ?? false;
+  // @ts-ignore
+  const pageParam = params?.page ?? false;
+
+  console.log("pageValue", pageValue === pageParam);
+  if (pageValue === pageParam) return;
+
+  console.log("name", name);
+  console.log("path", path);
+  console.log("params", name);
+  console.log("query", query);
+
+  if (pageValue) {
+    const pathWithoutPage = removePageFromUrl(path);
+    console.log("pathWithoutPage", pathWithoutPage);
+
+    const { page, ...queryTmp } = query;
+    const newFullPath = qs.stringify(queryTmp);
+    console.log("newFullPath", newFullPath);
+
+    const fullPath = `${pathWithoutPage}/page/${pageValue}${
+      newFullPath ? "?" + newFullPath : ""
+    }`;
+    console.log("fullPath of redirect", pathWithoutPage);
+    return navigateTo(fullPath, {
+      redirectCode: 301,
+      external: true,
+    });
+  }
+});
